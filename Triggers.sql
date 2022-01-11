4aa51eb2fe19277ed2db6f37529bef89b5dea636
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-01-11 19:41:21 -0400

    TRIGGER nacimiento del familiar

--- Triggers.sql
+++ Triggers.sql
@@ -32,19 +32,11 @@ BEFORE INSERT ON empleado_inteligencia FOR EACH ROW
 DECLARE 
 
 BEGIN
-    IF :NEW.familiar1.fec_nac > SYSDATE THEN 
+    IF ((:NEW.familiar1.fec_nac > SYSDATE) OR (:NEW.familiar2.fec_nac > SYSDATE)) THEN
         raise_application_error(-20900,'El familiar debe ser mayor de edad');
     END IF;
     
-    IF trunc(months_between((SYSDATE), :NEW.familiar1.fec_nac)/12) < 18 THEN
-        raise_application_error(-20900,'El familiar debe ser mayor de edad');
-    END IF;
-
-    IF :NEW.familiar2.fec_nac > SYSDATE THEN 
-        raise_application_error(-20900,'El familiar debe ser mayor de edad');
-    END IF;
-    
-    IF trunc(months_between((SYSDATE), :NEW.familiar2.fec_nac)/12) < 18 THEN
+    IF ((trunc(months_between((SYSDATE), :NEW.familiar1.fec_nac)/12) < 18) OR (trunc(months_between((SYSDATE), :NEW.familiar2.fec_nac)/12) < 18)) THEN
         raise_application_error(-20900,'El familiar debe ser mayor de edad');
     END IF;
     
