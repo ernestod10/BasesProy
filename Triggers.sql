6b849f8fd37b2b104ad85f2338ea51f73834e379
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-01-11 16:43:35 -0400

    TRIGGER EDAD FAMILIAR EMPLEADO

--- Triggers.sql
+++ Triggers.sql
@@ -24,3 +24,28 @@ BEGIN
     when lugar_incorrecto THEN  
     dbms_output.put_line('Oficina ubicada en la direccion de area equivocado');
 END; 
+
+
+--VALIDAR FECHA DE NACIMIENTO DE FAMILIAR DEL EMPLEADO
+create or replace TRIGGER VALIDA_FECHA_FAMILIAR
+BEFORE INSERT ON empleado_inteligencia FOR EACH ROW
+DECLARE 
+
+BEGIN
+    IF :NEW.familiar1.fec_nac > SYSDATE THEN 
+        raise_application_error(-20900,'El familiar debe ser mayor de edad');
+    END IF;
+    
+    IF trunc(months_between((SYSDATE), :NEW.familiar1.fec_nac)/12) < 18 THEN
+        raise_application_error(-20900,'El familiar debe ser mayor de edad');
+    END IF;
+
+    IF :NEW.familiar2.fec_nac > SYSDATE THEN 
+        raise_application_error(-20900,'El familiar debe ser mayor de edad');
+    END IF;
+    
+    IF trunc(months_between((SYSDATE), :NEW.familiar2.fec_nac)/12) < 18 THEN
+        raise_application_error(-20900,'El familiar debe ser mayor de edad');
+    END IF;
+    
+END;
