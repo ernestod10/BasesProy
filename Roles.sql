3b21fe70d54ea4031f70bcb8a4817c893db6e36b
Diego <diegobastardo1@gmail.com>
2022-01-13 21:29:26 -0400

    Creacion de Role D_area

--- Roles.sql
+++ Roles.sql
@@ -1,6 +1,6 @@
 
 -- Director_Area
-create role Director_Area;
-grant UPDATE(presupuesto_anual) on estacion to Director_Area; -- Para que el director de una estacion pueda actualizar (Debe existir un trigger que valide que dirige dicha estacion)
-grant select on estacion to Director_Area;
-grant select on empleado_inteligencia to Director_Area;
+create role D_area NOT IDENTIFIED;
+grant UPDATE(presupuesto_anual) on estacion to D_area; -- Para que el director de una estacion pueda actualizar (Debe existir un trigger que valide que dirige dicha estacion)
+grant select on estacion to D_area;
+grant select on empleado_inteligencia to D_area;
