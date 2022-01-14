ac93631347f5d38c901cafc0e999bb63e1ba5b0a
Ernesto <48799528+ernestod10@users.noreply.github.com>
2022-01-13 22:11:09 -0400

    Update Roles.sql

--- Roles.sql
+++ Roles.sql
@@ -4,3 +4,17 @@ create role D_area NOT IDENTIFIED;
 grant UPDATE(presupuesto_anual) on estacion to D_area; -- Para que el director de una estacion pueda actualizar (Debe existir un trigger que valide que dirige dicha estacion)
 grant select on estacion to D_area;
 grant select on empleado_inteligencia to D_area;
+
+--Analista
+create role Analista NOT IDENTIFIED;;
+grant select on hecho_crudo to Analista;
+grant update on hecho_crudo to Analista;
+grant select on pieza_inteligencia to Analista;
+grant update on pieza_inteligencia to Analista;
+grant select on p_h to Analista;
+grant update on p_h to Analista;
+grant select on p_t to Analista;
+grant update on p_t to Analista;
+grant select on verificacion_hecho to Analista;
+grant update on verificacion_hecho to Analista;
+
