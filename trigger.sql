d66c278e5269a9f190c79c68496b28c03f912fbc
Anthony Monsalve <22anthony.monsalve@gmail.com>
2022-01-13 22:32:30 -0400

    eliminé mis triggers de fecha de hecho crudo porque no convienen con las fechas que nos dio la profesora

--- trigger.sql
+++ trigger.sql
@@ -14,40 +14,14 @@ BEGIN
     
 END;
 
-
---VALIDAR FECHA DE PIEZA DE INTELIGENCIA (QUE NO SEA MAYOR A LA FECHA ACTUAL) 
-create or replace TRIGGER VALIDA_FECHA_PZ_INT
-BEFORE INSERT ON pieza_inteligencia FOR EACH ROW
+--VALIDAR EDAD MAYOR QUE 26 AÑOS DE ANALISTA Y AGENTE DE CAMPO
+create or replace TRIGGER VALIDA_EDAD_26
+BEFORE INSERT ON empleado_inteligencia FOR EACH ROW
 DECLARE 
 
 BEGIN
-    IF (:NEW.fecha_construccion > SYSDATE) THEN
-        raise_application_error(-20900,'La fecha de creación de pieza de inteligencia no puede ser mayor a la fecha actual');
-    END IF;
-    
-END;
-
---VALIDAR FECHA DE PIEZA DE INTELIGENCIA (QUE NO SEA MAYOR A LA FECHA ACTUAL) 
-create or replace TRIGGER VALIDA_FECHA_PZ_INT
-BEFORE INSERT ON pieza_inteligencia FOR EACH ROW
-DECLARE 
-
-BEGIN
-    IF (:NEW.fecha_construccion > SYSDATE) THEN
-        raise_application_error(-20900,'La fecha de creación de pieza de inteligencia no puede ser mayor a la fecha actual');
-    END IF;
-    
-END;
-
-
---VALIDAR FECHA DE HECHO CRUDO (QUE NO SEA MAYOR A LA FECHA ACTUAL) 
-create or replace TRIGGER VALIDA_FECHA_HECHO
-BEFORE INSERT ON hecho_crudo FOR EACH ROW
-DECLARE 
-
-BEGIN
-    IF ((:NEW.fec_obten > SYSDATE) OR (:NEW.fec_fin_cierre > SYSDATE)) THEN
-        raise_application_error(-20900,'La fecha de creación de HECHO CRUDO no puede ser mayor a la fecha actual');
+    IF (trunc(months_between((SYSDATE), :NEW.familiar1.fec_nac)/12) < 26) THEN
+        raise_application_error(-20900,'Debe ser mayor de 26 años');
     END IF;
     
 END;
