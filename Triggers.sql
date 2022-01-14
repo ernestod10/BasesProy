e0a60aee656e2a7db851c9407938c830356f1aea
Diego <diegobastardo1@gmail.com>
2022-01-14 18:53:19 -0400

    Trigger de validar mayor de edad

--- Triggers.sql
+++ Triggers.sql
@@ -76,6 +76,19 @@ BEGIN
     
 END;
 
+--VALIDAR EDAD MAYOR QUE 26 AÑOS DE ANALISTA Y AGENTE DE CAMPO
+create or replace TRIGGER VALIDA_EDAD_26
+BEFORE INSERT ON empleado_inteligencia FOR EACH ROW
+DECLARE 
+
+BEGIN
+    IF (trunc(months_between((SYSDATE), :NEW.familiar1.fec_nac)/12) < 26) THEN
+        raise_application_error(-20900,'Debe ser mayor de 26 años');
+    END IF;
+    
+END;
+
+
 --VALIDAR FUENTE HECHO CRUDO 
 create or replace TRIGGER VALIDA_FUENTE_SECRETA
 BEFORE INSERT ON hecho_crudo FOR EACH ROW
