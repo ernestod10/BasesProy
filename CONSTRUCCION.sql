d86b12dbd63cfb00a4e2b041cc0039feba77ecd9
Ernesto <48799528+ernestod10@users.noreply.github.com>
2022-01-18 01:45:49 -0400

    aa

--- CONSTRUCCION.sql
+++ CONSTRUCCION.sql
@@ -1,32 +1,63 @@
-CREATE or replace function CREA_PIEZA
-return NUMBER
-is id NUMBER;
-(
-  )
-BEGIN
-    INSERT INTO pieza_inteligencia (HCFI,EMP_ID,EST_ID,OFIC_ID,pieza_int_id,tema_   VALUES (_hist_cg_fec_ini,_hist_cg_emp_int_id,_hist_cg_est_id,_hist_cg_ofic_id,_pieza_int_id,_tema_id,_p_t_id,_p_h_id,_hecho_cdo_id,_v_hecho_cdo_id);
-END;
+
+
+
 
 
 
 CREATE or replace procedure agregar_hecho (
     id_hecho in Number,
-    id_pieza in Number,
-)
+    id_pieza in Number
+)is 
 begin
   insert into P_H (HECHO_CDO_ID,PIEZA_INT_ID)
-  values (id_hecho,id_pieza)
+  values (id_hecho,id_pieza);
 end;
 
-Create or Replace procedure PIERZA_CREACION (
 
-)
 
-create or replace function Nivel_acceso
-return NUMBER
-is nivel number;   
+
+Create or Replace procedure PIERZA_CREACION (id_h in number)
+is 
 begin
-SELECT TO_NUMBER(SUBSTR(GRANTED_ROLE, -1)) into nivel from USER_ROLE_PRIVS where USERNAME=user and GRANTED_ROLE like  'ACCESO%';
-    return nivel;
+  agregar_hecho(id_h,CREA_PIEZA());
+end;
+
+-- funciones  para obtener los datos para crear una pieza 
+create or replace function get_estacion_id
+return number
+is
+  v_estacion_id number;
+begin
+  select ESTacion_ID into v_estacion_id from historico_cargo where emp_int_id = SUBSTR(user,-1) and fec_fin=null;
+  return v_estacion_id;
 end;
 /
+create or replace function get_fecha_inicio
+return date
+is
+  v_fecha_inic date;
+begin
+  select FEC_INICIO into v_fecha_inic from historico_cargo where emp_int_id = SUBSTR(user,-1) and fec_fin=null;
+  return v_fecha_inic;
+end;
+/
+create or replace function get_oficina
+return number
+is
+  v_oficina number;
+begin
+  select EST_OFIC_PRIN_ID into v_oficina from historico_cargo where emp_int_id = SUBSTR(user,-1) and fec_fin=null;
+  return v_oficina;
+end;
+/
+CREATE or replace procedure CREA_PIEZA(
+PRECIO_APROXIMADO in number,
+NIVEL_CONFIABILIDAD in number,
+NIVEL_SEGURIDAD in number,
+TEMA_ID in number
+)is
+begin
+    insert into pieza_inteligencia (fecha_construccion,hist_cg_emp_int_id,hist_cg_est_id,hist_cg_fec_ini,hist_cg_ofic_id,nivel_confiabilidad,nivel_seguridad,precio_aproximado,tema_id)
+VALUES(SYSDATE,(TO_NUMBER(SUBSTR(user, -1))), get_estacion_id,get_fecha_inicio,get_oficina,NIVEL_CONFIABILIDAD,NIVEL_SEGURIDAD,PRECIO_APROXIMADO,TEMA_ID);
+
+END;
--- VENTA+SEGURIDAD.sql
+++ VENTA+SEGURIDAD.sql
@@ -32,6 +32,21 @@ begin
     end if;
 end;
 /
+ -- triger q revisa si el cliente es exclusivo 
+Create or replace trigger venta_exclusiva_trigger
+after insert on hist_venta
+
+for each row
+DECLARE ns number;
+
+begin 
+    Select exclusivo into ns from CLIENTE where id = :new.CLIENTE_ID ;
+    if ns = 1 then
+      -- aqui se ponen los delete cascades
+        
+    end if;
+end;
+/
 
 -- vista q muestra todas las piezas de inteligencia a las que el analista tiene acceso
 create view VER_PIEZAS 
