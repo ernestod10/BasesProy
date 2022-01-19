b3b6d01bb7d50c2282bcd10099757c4200ca8720
Ernesto <48799528+ernestod10@users.noreply.github.com>
2022-01-18 21:25:09 -0400

    Update CONSTRUCCION.sql

--- CONSTRUCCION.sql
+++ CONSTRUCCION.sql
@@ -35,6 +35,19 @@ begin
   return v_oficina;
 end;
 /
+-- crea la pieza
+CREATE or replace procedure CREA_PIEZA(
+PRECIO_APROXIMADO in number,
+NIVEL_CONFIABILIDAD in number,
+NIVEL_SEGURIDAD in number,
+TEMA_ID in number
+)is
+begin
+    insert into pieza_inteligencia (hist_cg_emp_int_id,hist_cg_est_id,hist_cg_fec_ini,hist_cg_ofic_id,nivel_confiabilidad,nivel_seguridad,precio_aproximado,tema_id)
+VALUES((TO_NUMBER(SUBSTR(user, -1))), get_estacion_id,get_fecha_inicio,get_oficina,NIVEL_CONFIABILIDAD,NIVEL_SEGURIDAD,PRECIO_APROXIMADO,TEMA_ID);
+
+END;
+--crea la pieza
 CREATE or replace procedure CREA_PIEZA(
 PRECIO_APROXIMADO in number,
 NIVEL_CONFIABILIDAD in number,
@@ -46,7 +59,6 @@ begin
 VALUES(SYSDATE,(TO_NUMBER(SUBSTR(user, -1))), get_estacion_id,get_fecha_inicio,get_oficina,NIVEL_CONFIABILIDAD,NIVEL_SEGURIDAD,PRECIO_APROXIMADO,TEMA_ID);
 
 END;
-
 
 Create or replace trigger hecho_confi_trigger
 before insert on P_H
@@ -75,3 +87,43 @@ begin
     end if;
 end;
 /
+
+
+create or replace function get_suma(id_pieza in number) RETURN number
+is
+  v_suma number;
+begin
+    select SUM(NIVEL_CONFI_FIN) into v_suma from hecho_crudo hc inner join p_h ph on  hc.ID_HECHO_CDO = ph.HECHO_CDO_ID and PIEZA_INT_ID=id_pieza; 
+  return v_suma;
+end;
+/
+
+
+create or replace function get_cont(id_pieza in number) RETURN number
+is
+  v_connt number;
+begin
+    select count(NIVEL_CONFI_FIN) into v_connt from hecho_crudo hc inner join p_h ph on  hc.ID_HECHO_CDO = ph.HECHO_CDO_ID and PIEZA_INT_ID=id_pieza; 
+  return v_connt;
+end;
+/
+
+CREATE OR REPLACE TRIGGER promedio
+    after INSERT ON P_H
+    FOR EACH ROW 
+BEGIN
+    UPDATE pieza_inteligencia 
+    SET NIVEL_CONFIABILIDAD = (get_suma(:new.PIEZA_INT_ID)/get_cont(:new.PIEZA_INT_ID)) 
+    where ID=:new.PIEZA_INT_ID;
+
+END;
+/
+
+
+
+ CREATE OR REPLACE TRIGGER pieza_fin
+    before INSERT ON P_H
+    FOR EACH ROW 
+BEGIN
+    
+END;
