586d2fde90ab76b81298c1ba513dbac1768042e4
Diego <diegobastardo1@gmail.com>
2022-01-19 10:32:29 -0400

    cambios varios

--- roles_cuenta.sql
+++ roles_cuenta.sql
@@ -8,7 +8,7 @@ grant select any table to D_CEO;
 -- Director_Area
 create role D_area NOT IDENTIFIED;
 grant UPDATE(presupuesto_anual) on estacion to D_area; -- Para que el director de una estacion pueda actualizar (Debe existir un trigger que valide que dirige dicha estacion)
-grant select,insert on estacion to D_area;
+grant select,insert,update on estacion to D_area;
 grant select on empleado_inteligencia to D_area;
 grant delete on empleado_inteligencia to D_area;
 grant select on hecho_crudo to D_area;
